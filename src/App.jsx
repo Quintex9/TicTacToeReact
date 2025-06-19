ea7fa87059b1f26c7906073ff158a651f31f4bf6
Quintex9 <misko.smatana@gmail.com>
2025-06-19 16:22:59 +0200

    Vytvorenie GameBoard.jsx, dynamické tlačidlá, pridanie GameBoard do App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import GameBoard from "./components/GameBoard"
 import Player from "./components/Player"
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
           <Player name="Player1" symbol="X"/>
           <Player name="Player2" symbol="O"/>
         </ol>
+        <GameBoard/>
       </div>
     </main>
   )
