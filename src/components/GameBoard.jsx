3b85ba9136acbba0abec7057b16fd43518857ebd
Quintex9 <misko.smatana@gmail.com>
2025-06-20 07:57:37 +0200

    Dokončenie gameboard

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,17 +7,27 @@ const initialGameBoard = [
 ];
 
 export default function GameBoard() {
+  const [gameBoard, setGameBoard] = useState(initialGameBoard);
+
+  function changeButton(rowIndex, colIndex) {
+    setGameBoard((prevGameBoard) => {
+      const updatedBoard = [...prevGameBoard.map(innerArray => [...innerArray])];
+      updatedBoard[rowIndex][colIndex] = "X";
+      return updatedBoard;
+    });
+  }
+
   return (
     <ol id="game-board">
-      {initialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
-            <ol>
-                {row.map((playerSymbol, colIndex) => <li>
-                    <button>
-                        {playerSymbol}
-                    </button>
-                </li>)}
-            </ol>
+          <ol>
+            {row.map((playerSymbol, colIndex) => (
+              <li>
+                <button onClick={() => changeButton(rowIndex,colIndex)}>{playerSymbol}</button>
+              </li>
+            ))}
+          </ol>
         </li>
       ))}
     </ol>
