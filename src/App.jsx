8276849b2b741ab803cb6658c003ce8386260fbd
Quintex9 <misko.smatana@gmail.com>
2025-06-23 08:50:11 +0200

    update tlačidla v GameBoard, zablokovanie možnosti kliknúť viackrát

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,50 @@
-import { useState } from "react"
-import GameBoard from "./components/GameBoard"
-import Player from "./components/Player"
+import { useState } from "react";
+import GameBoard from "./components/GameBoard";
+import Player from "./components/Player";
 import Log from "./components/Log";
+
+function deriveActivePlayer(gameTurns){
+  let currentPlayer = "X";
+
+  if (gameTurns.length > 0 && gameTurns[0].player === "X") {
+    currentPlayer = "O";
+  }
+  
+  return currentPlayer;
+}
 
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
-  const [activePlayer, setActivePlayer] = useState("X");
+  //const [activePlayer, setActivePlayer] = useState("X");
 
-  function handleSelectSquare(rowIndex,colIndex){
-    setActivePlayer((curActivePlayer) => curActivePlayer === "X" ? "O" : "X");
-    setGameTurns(prevTurns =>{
-      let currentPlayer = "X";
+  const activePlayer = deriveActivePlayer(gameTurns);
 
-      if (prevTurns.length > 0 && prevTurns[0].player === "X"){
-        currentPlayer = "O";
-      }
+  function handleSelectSquare(rowIndex, colIndex) {
+    //setActivePlayer((curActivePlayer) => curActivePlayer === "X" ? "O" : "X");
+    setGameTurns((prevTurns) => {
+      const currentPlayer = deriveActivePlayer(prevTurns);
 
-
-      const updatedTurns = [{square: {row: rowIndex, col: colIndex}, player: currentPlayer} , ...prevTurns];
+      const updatedTurns = [
+        { square: { row: rowIndex, col: colIndex }, player: currentPlayer },
+        ...prevTurns,
+      ];
 
       return updatedTurns;
-    })
+    });
   }
 
   return (
     <main>
       <div id="game-container">
         <ol id="players" className="highlight-player">
-          <Player name="Player1" symbol="X" isActive={activePlayer === "X"}/>
-          <Player name="Player2" symbol="O" isActive={activePlayer === "O"}/>
+          <Player name="Player1" symbol="X" isActive={activePlayer === "X"} />
+          <Player name="Player2" symbol="O" isActive={activePlayer === "O"} />
         </ol>
-        <GameBoard 
-          onSelectSquare={handleSelectSquare} 
-          turns={gameTurns}/>
+        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
       </div>
-      <Log turns={gameTurns}/>
+      <Log turns={gameTurns} />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
